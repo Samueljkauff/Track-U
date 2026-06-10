26be2c290abfa98e94a3828060416635dd323a49
Sam Kauffman <samueljkauff@gmail.com>
2026-06-10 03:26:06 -0400

    Install tailwind

--- Track-U/vite.config.ts
+++ Track-U/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
 import inertia from '@adonisjs/inertia/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
     inertia({ ssr: { enabled: false, entrypoint: 'inertia/ssr.ts' } }),
     adonisjs({ entrypoints: ['inertia/app.ts'], reload: ['resources/views/**/*.edge'] }),
