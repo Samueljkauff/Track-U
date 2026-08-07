1432674667d850530a9e5a1695c1c07905dfb8ab
Sam Kauffman <samueljkauff@gmail.com>
2026-08-07 02:49:24 -0400

    Small changes

--- Track-U/inertia/components/Topsters.vue
+++ Track-U/inertia/components/Topsters.vue
@@ -2,13 +2,13 @@
         <div class="h-auto col-span-3 flex flex-col items-center gap-3 mr-3 ml-3 mb-3 text-[#f8f9fa]">
             <div v-for="topster, i in Categories" class="flex flex-col items-center font-semibold p-3 text-lg h-full container-shadow bg-(--surface) rounded-xs">
                 {{ topster.name }}
-                <div v-for="(item, index) in topster.topThree" :key="index" class="w-full h-full grid grid-cols-3">
+                <div v-for="(item, index) in topster.topThree" :key="index" class="w-[75%] h-full grid grid-cols-3">
                     <hr class="col-span-3 m-2">
                     <p>{{ index + 1 }}</p>
                     <img alt="img" src="">
                     <div class="flex flex-col">
                         <p>{{ item.name }}</p>
-                        <p>{{ item.plays }}</p>
+                        <p>{{ item.plays }} plays</p>
                     </div>
                 </div>
             </div>
