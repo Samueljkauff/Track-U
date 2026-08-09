537ce00804e2f9ed2f014dd8a485cf94e9c15ae8
Sam Kauffman <samueljkauff@gmail.com>
2026-08-09 14:57:54 -0400

    DotNav work

--- Track-U/inertia/components/DotNav.vue
+++ Track-U/inertia/components/DotNav.vue
@@ -3,13 +3,20 @@
         <div
             v-for="i in tabNum"
             :key="i"
-            class="w-2 h-2 flex bg-[#212529] rounded-md m-1 hover:cursor-pointer"
+            class="w-2 h-2 rounded-md m-1 hover:cursor-pointer"
+            :class="selected === i ? 'bg-[#212529]' : 'bg-[#454d51]'"
+            @click="selected = i"
         ></div>
     </div>
 </template>
 
 <script lang="ts">
 export default {
+    data() {
+        return {
+            selected: 1,
+        }
+    },
     props: {
         tabNum: Number
     }
