58ba4d424899fae672c493c751a095e34afde709
Sam Kauffman <samueljkauff@gmail.com>
2026-08-09 15:00:05 -0400

    DotNav animation volot

--- Track-U/inertia/components/DotNav.vue
+++ Track-U/inertia/components/DotNav.vue
@@ -3,7 +3,7 @@
         <div
             v-for="i in tabNum"
             :key="i"
-            class="w-2 h-2 rounded-md m-1 hover:cursor-pointer"
+            class="w-2 h-2 rounded-md m-1 hover:cursor-pointer transition-colors duration-300"
             :class="selected === i ? 'bg-[#212529]' : 'bg-[#454d51]'"
             @click="selected = i"
         ></div>
