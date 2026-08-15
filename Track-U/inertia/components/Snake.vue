17e15df663f518c7497a575ee5a7a76086342584
Sam Kauffman <samueljkauff@gmail.com>
2026-08-14 23:20:20 -0400

    Started snake logic

--- Track-U/inertia/components/Snake.vue
+++ Track-U/inertia/components/Snake.vue
@@ -1,5 +1,28 @@
 <template>
     <div class="h-full w-full grid grid-cols-10 pt-8 pb-8">
-        <div v-for="i in 10*17" class="border"></div>
+        <div v-for="i in 170" :class="i === appleCords ? 'bg-[#82302a]' : ''" class="border">{{ appleCords }}</div>
     </div>
 </template>
+
+<script lang="ts">
+    export default {
+        data() {
+            return {
+                appleCords: 0,
+            }
+        },
+        mounted() {
+            this.startGameLoop();
+        },
+        methods: {
+            getRandomInt(min: number, max: number): number {
+                const minCeiled = Math.ceil(min);
+                const maxFloored = Math.floor(max);
+                return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
+            },
+            startGameLoop() {
+                this.appleCords = this.getRandomInt(1, 170);
+            }
+        }
+    }
+</script>
