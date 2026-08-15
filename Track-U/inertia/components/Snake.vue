640621eb3c63ea13bcafa7a1f03ced8e1b5ab91e
Sam Kauffman <samueljkauff@gmail.com>
2026-08-15 03:36:22 -0400

    Collision logic and etc

--- Track-U/inertia/components/Snake.vue
+++ Track-U/inertia/components/Snake.vue
@@ -3,10 +3,16 @@
         <div v-for="i in boardSize" :class="
         i === appleCords
             ? 'bg-[#82302a]'
-            : i === snakeSpawn
+            : i === snakeLocation
                 ? 'bg-black'
                 : ''
     " class="border"></div>
+        <div
+            v-if="!alive"
+            class="absolute z-10 bg-white px-4 py-2"
+        >
+            Game Over
+        </div>
     </div>
 </template>
 
@@ -16,10 +22,14 @@
             return {
                 boardSize: 170,
                 appleCords: 0,
-                snakeSpawn: 0,
+                snakeLocation: 0,
+                direction: -10,
+                gameLoop: null as ReturnType<typeof setInterval> | null,
+                alive: true,
             }
         },
         mounted() {
+            window.addEventListener('keydown', this.handleMovement);
             this.startGameLoop();
         },
         methods: {
@@ -30,8 +40,50 @@
             },
             startGameLoop() {
                 this.appleCords = this.getRandomInt(1, 170);
-                this.snakeSpawn = this.boardSize/2;
-            }
-        }
-    }
+                this.snakeLocation = this.boardSize/2; 
+                this.alive = true;
+
+                this.gameLoop = setInterval(()=> {
+                    let nextLocation = this.snakeLocation + this.direction;
+                    if(!this.isPositionValid()) {
+                        this.alive = false;
+                    } else {
+                        this.snakeLocation = nextLocation;
+                    }
+                }, 180)
+
+                if(!this.alive) {
+                    clearInterval(this.gameLoop);
+                }
+            },
+            handleMovement(Event: KeyboardEvent) {
+                switch(Event.key) {
+                    case 'ArrowUp':
+                        this.direction = -10;
+                        break;
+                    case 'ArrowDown':
+                        this.direction = 10;
+                        break;
+                    case 'ArrowLeft':
+                        this.direction = -1;
+                        break;
+                    case 'ArrowRight':
+                        this.direction = 1;
+                        break;
+                    default:
+                        break;
+                }
+            },
+            isPositionValid(): boolean {
+                let nextLocation = this.snakeLocation + this.direction;
+                let isValid = true;
+
+                if(nextLocation > 170 || nextLocation < 1 || (this.snakeLocation%10 === 0 && nextLocation%10 === 1) || (nextLocation%10 === 0 && this.snakeLocation%10 === 1)) {
+                    isValid = false;
+                }
+
+                return isValid;
+            },
+    },
+}
 </script>
